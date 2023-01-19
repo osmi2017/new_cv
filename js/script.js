642f372b27187178d94a14ed0b8062b4451b7d22
Soro Ndianhan Brahima Ismael <soro.ismael@financialxs.com>
2023-01-19 19:42:35 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundImage = "url('https://www.toptal.com/designers/subtlepatterns/uploads/hypnotize.png')";
+
+document.body.style.backgroundAttachment = "fixed";
+
+document.body.style.backgroundPosition = "center";
+
